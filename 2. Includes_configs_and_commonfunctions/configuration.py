551df957fd3b7e59590462465cf7dde9fb766d3b
Eswara Reddy Thimmapuram <119644268+Eshwarreddyt@users.noreply.github.com>
2023-07-23 21:25:29 -0500

    first commit

---
+++ 2. Includes_configs_and_commonfunctions/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = 'abfss://raw@erformula1dl.dfs.core.windows.net'
+processed_folder_path = 'abfss://processed@erformula1dl.dfs.core.windows.net'
+presentation_folder_path = 'abfss://presentation@erformula1dl.dfs.core.windows.net'
+
+# COMMAND ----------
+
